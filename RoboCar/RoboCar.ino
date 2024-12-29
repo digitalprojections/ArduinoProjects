96511ee14683b03fb79de1b7bb5117f0622fcd39
Ahmad Fuzalov <36181294+digitalprojections@users.noreply.github.com>
2024-12-29 21:46:38 +0900

    Update RoboCar.ino

--- RoboCar/RoboCar.ino
+++ RoboCar/RoboCar.ino
@@ -2,12 +2,17 @@
 #include <Adafruit_ST7789.h>  // Hardware-specific library for ST7789
 #include <SPI.h>
 #include "Adafruit_ST77xx.h"
+#include <Adafruit_PCF8574.h>
+
+
 
 #define TFT_CS 8    // if your display has CS pin
 #define TFT_RST 10  // reset pin
 #define TFT_DC 9    // data pin
 #define TFT_MOSI 11
 #define TFT_SCLK 13
+
+Adafruit_PCF8574 pcf;
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
@@ -25,7 +30,18 @@ void setup(void) {
   tft.setCursor(20, 110);
   tft.fillRect(0, 105, 240, 40, ST77XX_ORANGE);
   tft.println("You Won!");
+
+  if (!pcf.begin(0x20, &Wire)) {
+    Serial.println("Couldn't find PCF8574");
+    while (1);
+  }
+  pcf.pinMode(7, OUTPUT);
+  Serial.println("Could find PCF8574");
 }
 
 void loop() {
+  pcf.digitalWrite(7, LOW);  // turn LED on by sinking current to ground
+  delay(100);
+  pcf.digitalWrite(7, HIGH); // turn LED off by turning off sinking transistor
+  delay(100);
 }
